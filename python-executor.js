9786b48ce9b1c5e17b26ce3e77a19bacd853c9b6
Creambeam <andy.gallagher0714@gmail.com>
2025-03-24 13:56:21 -0500

    Create python-executor.js

---
+++ python-executor.js
@@ -0,0 +1,8 @@
+const executePythonCode = async (code) => {
+  // Use a service like Repl.it's API or PythonAnywhere's API to execute the Python code
+  // ...
+  const output = await executeCode(code);
+  return output;
+};
+
+export default executePythonCode;
